340731f94444996a32d556a4f39919a63ab5b13a
Antonella Enriquez Ledesma <antonellaenriquezledesma@gmail.com>
2024-07-05 23:16:46 +0200

    git chectabla finalizada

--- Readme.md
+++ Readme.md
@@ -13,7 +13,7 @@ Utilizando fetch API y async/await de javascript imprime en la consola del naveg
 recurso utilizado (https://dev.to/rogerdimonte/como-conectarse-a-una-api-async-await-fetch-3ekn)
 
 Crea una tabla e imprime todos los usuarios con su ID, nombre y la ciudad donde reside.
-
+![alt text](image-1.png)
 
 
 Crea un input con su botón para que el usuario de tu aplicación pueda solicitar mediante el ID los datos de un solo usuario. Hecha la solicitud, imprime en pantalla el nombre y el teléfono del usuario solicitado.
--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const llamandoAPI = async () => {
-    try {
-      const respuesta = await fetch('https://jsonplaceholder.typicode.com/users')
-  
-      const data =  respuesta.json()
-      console.log(data)
-  
-    } catch (error) {
-      console.log(error);
-    } 
+  try {
+    const respuesta = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await respuesta.json(); 
+    
+    const tbody = document.getElementById('usuarios').getElementsByTagName('tbody')[0];
+
+    data.forEach(user => {
+      const fila = document.createElement('tr');
+
+      const celdaID = document.createElement('td');
+      celdaID.textContent = user.id;
+      fila.appendChild(celdaID);
+
+      const celdaNombre = document.createElement('td');
+      celdaNombre.textContent = user.name;
+      fila.appendChild(celdaNombre);
+
+      const celdaCiudad = document.createElement('td');
+      celdaCiudad.textContent = user.address.city;
+      fila.appendChild(celdaCiudad);
+
+      tbody.appendChild(fila);
+    });
+  } catch (error) {
+    console.log(error);
   }
-  
-  llamandoAPI()
+}
+
+llamandoAPI();
